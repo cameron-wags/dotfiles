aa75a941212ee374ea5c42d243e20fbbedc73a55
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-06-10 01:16:02 -0400

    split parse loop from parser state checks

--- installspec.sh
+++ installspec.sh
@@ -6,10 +6,29 @@ section=""
 
 paclist="pac.list"
 aurlist="aur.list"
+# It's a good idea to empty these before we regenerate them.
 [ -f "$paclist" ] && rm pac.list
 [ -f "$aurlist" ] && rm aur.list
 touch pac.list aur.list
 
+applyState() {
+    [ "$machine" = "$host" -o "$machine" = "global" ] || return 0
+
+    case $section in
+        excludes)
+            # rm -f $line
+            echo -e "$machine\tdelete\t$1"
+            ;;
+        wants)
+            echo "$1" >> "$paclist"
+            ;;
+        gitwants)
+            echo "$1" >> "$aurlist"
+            ;;
+    esac
+}
+
+# spec.txt parse loop
 sed -E 's/\s*#.*//g; s/^\s*//g; s/\s*$//g; /^$/d' spec.txt | \
 while read line; do
     # replace this regex match with a non-bashism
@@ -18,22 +37,11 @@ while read line; do
     elif [[ "$line" =~ ^:machine ]]; then
         machine=$(echo "$line" | sed -E 's/^:machine\s+(\w+)$/\1/')
     else
-        [ "$machine" = "$host" -o "$machine" = "global" ] && \
-        case $section in
-            excludes)
-                # rm -f $line
-                echo -e "$machine\tdelete\t$line"
-                ;;
-            wants)
-                echo "$line" >> "$paclist"
-                ;;
-            gitwants)
-                echo "$line" >> "$aurlist"
-                ;;
-        esac
+        applyState "$line"
     fi
 done
 
+exit 0
 cat "$paclist" | sudo pacman -S --needed -
 
 cat "$aurlist" | aur -i -
