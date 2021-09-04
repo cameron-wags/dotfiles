4e7ee96224d855e17cd420a2db469fcffb51b0da
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-09-03 20:18:41 -0400

    caffiene binding notifies

--- bin/xsettoggle.sh
+++ bin/xsettoggle.sh
@@ -1,9 +1,11 @@
-#!/bin/dash
+#!/bin/sh
 
 if [ $(xssstate -s) = "disabled" ]; then
 	xset s 300
+    notify-send -t 1500 -u normal "CAFFIENE OFF" "GOOD NIGHT"
 else
 	xset -dpms
 	xset s off
+    notify-send -t 1500 -u normal "CAFFIENE ON" "NO SLEEPING, OK?"
 fi
 
