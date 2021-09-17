3b9ac5828a787f4a51573959462269de6fd0ff43
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-09-16 21:57:50 -0400

    Use coffee bg

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -9,7 +9,7 @@ local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
-local wallpaper_path = "~/.config/awesome/background.jpg"
+local wallpaper_path = "~/.config/awesome/background.png"
 
 local theme = {}
 
