8137a405b47882140b70940a6d8a0a075457c375
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-10-01 07:53:26 -0400

    Fail loudly when pacman can't find something
    Also make the tailscale thing only pop up when it's needed

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 # vim:foldmethod=marker
 
+die() {
+    echo -e "$1"
+    exit 1
+}
+
 if [ $(whoami)  = "root" ]; then
-    echo "don't run as root"
-    exit 0
+    die "don't run as root"
 fi
 
 ls -al ~/.ssh | grep -qi \.pub || {
-    echo -e "Set up a GitHub PAT\nRun: ssh-keygen -t ed25519 -C \"email@example.com\""
-    exit 1
+    die "Set up a GitHub PAT\nRun: ssh-keygen -t ed25519 -C \"email@example.com\""
 }
 
 cd "$HOME/repo/linux-conf"
@@ -22,7 +25,7 @@ read -p "Install spec'd packages? [y/N]: " -n 1 answer
 echo ""
 
 if [ "$answer" = "Y" -o "$answer" = "y" ]; then
-    cat "pac.list" | sudo pacman -S --needed -
+    cat "pac.list" | sudo pacman -S --needed - || die "pacman failed, maybe fix spec.txt?"
 
     mkdir -p ~/aur
     cat "aur.list" | ./aur.sh -i -
@@ -31,12 +34,14 @@ fi
 [ -h ~/.zprofile ] || ln -s ~/.config/shell/profile ~/.zprofile
 
 # Tailscale maps
-echo -e "\nLog into TailScale before answering this!"
-read -p "Add tailscale IPs to /etc/hosts? [y/N]: " -n 1 answer
-echo ""
-if [ "$answer" = "Y" -o "$answer" = "y" ]; then
-    tailscale status | \
-    sed -E 's/\s{2,}/ /g' | \
-    cut -d' ' -f1,2 | \
-    sudo tee -a /etc/hosts
-fi
+grep "$(tailscale status | sed -E 's/\s{2,}/ /g' | cut -d' ' -f1,2)" /etc/hosts 2>&1 > /dev/null || {
+    echo -e "\nLog into TailScale before answering this!"
+    read -p "Add tailscale IPs to /etc/hosts? [y/N]: " -n 1 answer
+    echo ""
+    if [ "$answer" = "Y" -o "$answer" = "y" ]; then
+        tailscale status | \
+        sed -E 's/\s{2,}/ /g' | \
+        cut -d' ' -f1,2 | \
+        sudo tee -a /etc/hosts
+    fi
+}
