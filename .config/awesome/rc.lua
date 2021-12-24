13f5e97ec5e8a7da49fda3d875dd424dddd8b52f
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-12-24 14:46:50 -0600

    florence on-screen kb

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -645,6 +645,7 @@ awful.rules.rules = {
           "Gpick",
           "Kruler",
           "MessageWin",  -- kalarm.
+          "florence"
           "Sxiv",
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
