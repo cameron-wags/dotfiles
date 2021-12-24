85e550109dffad949b6cff2f541833945e1762a1
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-12-24 15:02:18 -0600

    dropped comma

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -645,7 +645,7 @@ awful.rules.rules = {
           "Gpick",
           "Kruler",
           "MessageWin",  -- kalarm.
-          "florence"
+          "florence",
           "Sxiv",
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
