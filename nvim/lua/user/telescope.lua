df3f3d345fbfc91d6c7050ad4f87246dc737b1f9
Cameron Wagstaff <cam.wagstaff@gmail.com>
2022-08-22 21:38:36 -0500

    anyway

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -4,6 +4,22 @@ if not telescope_ok then
 end
 
 telescope.setup {
+  defaults = {
+    layout_strategy = 'flex',
+    layout_config = {
+      flex = {
+        prompt_position = 'bottom',
+        flip_columns = 180,
+        flip_lines = 60,
+        horizontal = {
+          preview_width = 0.6,
+        },
+        vertical = {
+          preview_height = 0.45,
+        },
+      },
+    },
+  },
   extensions = {
     fzy_native = {
       override_generic_sorter = true,
