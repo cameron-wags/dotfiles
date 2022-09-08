ebdd49a97218a68bab0b6af5c2c7c03133ed6085
Cameron Wagstaff <cam.wagstaff@gmail.com>
2022-09-07 22:14:50 -0500

    version splash

--- nvim/lua/user/splash.lua
+++ nvim/lua/user/splash.lua
@@ -1,23 +1,15 @@
+local version = vim.api.nvim_exec(':version', true)
+local maxlen = 0
+local lines = {}
+for line in string.gmatch(version, '([^\r\n]+)') do
+  local l = string.len(line)
+  if l > maxlen then
+    maxlen = l
+  end
+  table.insert(lines, line)
+end
+
 require 'splash'.setup {
-  text = {
-    '     ▒▒▒▒             ▒▒     ',
-    '   ▒▒▒▒▒▒▒            ▒▒▒▒   ',
-    '  ▒▒▒▒▒▒▒▒▒           ▒▒▒▒▒▒ ',
-    '░░ ▒▒▒▒▒▒▒▒▒░         ▒▒▒▒▒▒▒',
-    '░░░ ▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒',
-    '░░░░░ ▒▒▒▒▒▒▒▒▒       ▒▒▒▒▒▒▒',
-    '░░░░░░ ▒▒▒▒▒▒▒▒▒░     ▒▒▒▒▒▒▒',
-    '░░░░░░░ ░▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒',
-    '░░░░░░░   ▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒',
-    '░░░░░░░    ▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒',
-    '░░░░░░░     ▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒',
-    '░░░░░░░       ▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒',
-    '░░░░░░░        ▒▒▒▒▒▒▒▒▒▒ ▒▒▒',
-    '░░░░░░░          ▒▒▒▒▒▒▒▒▒ ▒▒',
-    ' ░░░░░░           ▒▒▒▒▒▒▒▒▒  ',
-    '   ░░░░            ▒▒▒▒▒▒▒   ',
-    '     ░░              ▒▒▒     ',
-    '                             ',
-  },
-  text_width = 29,
+  text = lines,
+  text_width = maxlen,
 }
