979ef79548938ba0dcfc60294d88e7f1f169a930
Cameron Wagstaff <cam.wagstaff@gmail.com>
2023-02-20 20:40:37 -0600

    lualine cleanup

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,38 +1,18 @@
-local symbols = {
-	unix = '',
-	dos = ' ',
-	mac = ' ',
-}
-
--- Tell me if the file isn't utf-8 or unix line endings.
-local fileinfo_ignore_norms = function()
-	local ret, _ = (vim.bo.fenc or vim.go.enc):gsub('^utf%-8$', '')
-	local fmt = vim.bo.fileformat
-	ret = ret .. (symbols[fmt] or fmt)
-	return ret
-end
-
 return {
 	'nvim-lualine/lualine.nvim',
 	lazy = false,
 	dependencies = { 'kyazdani42/nvim-web-devicons' },
 	opts = {
 		options = {
-			theme = 'auto',
-			icons_enabled = true,
 			component_separators = { left = '', right = '' },
 			section_separators = { left = '', right = '' },
-			disabled_filetypes = { 'NvimTree', 'FTerm', 'help' },
+			disabled_filetypes = { 'NvimTree', 'FTerm', 'help', 'startify' },
 			globalstatus = false,
 		},
 		sections = {
 			lualine_a = { 'mode' },
 			lualine_b = {
 				{ 'branch' },
-				{
-					'diff',
-					symbols = { added = '+', modified = '~', removed = '-' },
-				},
 			},
 			lualine_c = {
 				{ 'filename', path = 1 },
@@ -40,18 +20,31 @@ return {
 			lualine_x = {
 				{
 					'diagnostics',
-					sources = { 'nvim_lsp' },
-					update_in_insert = false,
-					always_visible = false,
+					sources = { 'nvim_diagnostic' },
 					symbols = {
 						error = '✗',
 						warn = '!',
 						info = '',
 						hint = '',
 					},
-					-- padding = { left = 1, right = 2 },
 				},
-				fileinfo_ignore_norms,
+				{
+					'encoding',
+					fmt = function(enc)
+						if enc ~= 'utf-8' then
+							return enc
+						end
+						return ''
+					end
+				},
+				{
+					'fileformat',
+					symbols = {
+						unix = '',
+						dos = ' ',
+						mac = ' ',
+					}
+				},
 				'filetype',
 			},
 			lualine_y = {
@@ -64,29 +57,19 @@ return {
 			lualine_a = {
 				{ 'filename', path = 1 }
 			},
-			lualine_b = {},
-			lualine_c = {},
 			lualine_x = { 'location' },
-			lualine_y = {},
-			lualine_z = {},
 		},
-		extensions = { 'nvim-tree' },
-		winbar = {
+		tabline = {
 			lualine_a = {
 				{
 					'buffers',
-					show_filename_only = true, -- Shows shortened relative path when set to false.
-					hide_filename_extension = false, -- Hide filename extension when set to true.
-					show_modified_status = true, -- Shows indicator when the buffer is modified.
 
-					mode = 0,
+					max_length = vim.o.columns,
 
-					max_length = function() return vim.api.nvim_win_get_width(0) end, -- Maximum width of buffers component,
-					-- it can also be a function that returns
-					-- the value of `max_length` dynamically.
 					filetype_names = {
 						TelescopePrompt = 'Telescope',
 						packer = 'Packer',
+						NvimTree = 'NvimTree',
 					},
 
 					padding = 1,
@@ -98,20 +81,6 @@ return {
 					},
 				},
 			},
-			lualine_b = {},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = {},
-		},
-		tabline = {},
-		inactive_tabline = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = {},
 		},
 	}
 }
