1a8e098f25cfdb77b1f2862936cb9b50d3902dd0
Cameron Wagstaff <cam.wagstaff@gmail.com>
2023-03-14 21:26:52 -0500

    fix nerd font glyph widths

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,22 +9,30 @@ local get_color_scheme = function(appearance)
 end
 
 return {
-	use_ime = false,
+	allow_square_glyphs_to_overflow_width = 'Never',
+	animation_fps = 1,
 	color_scheme = get_color_scheme(wezterm.gui.get_appearance()),
-	force_reverse_window_cursor = false,
-	cursor_blink_rate = 325,
 	cursor_blink_ease_in = 'Constant',
 	cursor_blink_ease_out = 'Constant',
-	harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-	font_size = 11.0,
-	window_frame = {
-		font_size = 11.5,
-	},
-	hide_tab_bar_if_only_one_tab = true,
-	use_fancy_tab_bar = false,
-	tab_max_width = 45,
-	window_decorations = 'RESIZE',
+	cursor_blink_rate = 325,
 	enable_scroll_bar = false,
+	font = wezterm.font_with_fallback {
+		'JetBrains Mono',
+		{ family = 'Noto Color Emoji',       scale = 1.0 },
+		{ family = 'Symbols Nerd Font Mono', scale = 0.65 },
+	},
+	font_size = 11.0,
+	force_reverse_video_cursor = false,
+	harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+	hide_tab_bar_if_only_one_tab = true,
+	initial_cols = 130,
+	initial_rows = 70,
+	show_new_tab_button_in_tab_bar = false,
+	tab_max_width = 45,
+	use_fancy_tab_bar = false,
+	use_ime = false,
+	window_decorations = 'RESIZE',
+	window_frame = { font_size = 11.5, },
 	window_padding = {
 		top = 0,
 		right = 0,
@@ -41,6 +49,18 @@ return {
 			key = 'j',
 			mods = 'CMD',
 			action = wezterm.action.ActivateTabRelative(1),
+		},
+		{
+			key = 't',
+			mods = 'CMD|SHIFT',
+			action = wezterm.action.SpawnTab('CurrentPaneDomain'),
+		},
+		{
+			key = 't',
+			mods = 'CMD',
+			action = wezterm.action.SpawnCommandInNewTab {
+				cwd = wezterm.home_dir,
+			},
 		},
 	}
 }
