b14cda6387d86631d315dd88904a53e40986e650
Cameron Wagstaff <cam.wagstaff@gmail.com>
2023-11-04 09:07:37 -0500

    none-ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -125,7 +125,7 @@ return {
 		end,
 	},
 	{
-		'jose-elias-alvarez/null-ls.nvim',
+		'nvimtools/none-ls.nvim',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		event = 'VeryLazy',
 		priority = 49,
