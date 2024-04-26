27942d1bd4818a3a16aa03de5d33ceb1510884f6
Cameron Wagstaff <cam.wagstaff@gmail.com>
2024-04-26 12:26:30 -0500

    refactor lspconfig custom setup away from mason

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,103 @@
+local ensure_installed = {
+	'prettierd',
+	'jsonls',
+	'pyright',
+	'shfmt',
+	'lua-language-server',
+	'typescript-language-server',
+	'pyright',
+	'ruff',
+}
+
+-- used by pyright
+-- this could potentially be in the python setup handler,
+-- need to research when those are invoked.
+local root_dir_cache = {}
+local pipenv_venv_cached = function(root_dir)
+	if root_dir_cache[root_dir] then
+		return root_dir_cache[root_dir]
+	end
+	local value = vim.fn.trim(vim.system({ 'pipenv', '--venv', '--quiet' }, { text = true }):wait().stdout)
+	root_dir_cache[root_dir] = value
+	return value
+end
+
+local custom_lspconfig = {
+	lua_ls = function(mix_capabilities)
+		local runtime_path = vim.split(package.path, ';')
+		table.insert(runtime_path, 'lua/?.lua')
+		table.insert(runtime_path, 'lua/?/init.lua')
+
+		require 'lspconfig'.lua_ls.setup(mix_capabilities {
+			settings = {
+				Lua = {
+					telemetry = { enable = false },
+					runtime = {
+						version = 'LuaJIT',
+						path = runtime_path,
+					},
+					diagnostics = {
+						globals = { 'vim' }
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.fn.expand('$VIMRUNTIME/lua'),
+							vim.fn.stdpath('config') .. '/lua'
+						}
+					}
+				}
+			}
+		})
+	end,
+
+	pyright = function(mix_capabilities)
+		-- condensed form of this:
+		-- https://github.com/younger-1/nvim/blob/one/lua/young/lsp/providers/pyright.lua
+		require 'lspconfig'.pyright.setup(mix_capabilities {
+			settings = {
+				python = {
+					analysis = {
+						useLibraryCodeForTypes = true,
+						autoImportCompletions = true,
+						autoSearchPaths = true,
+					},
+				},
+			},
+			on_new_config = vim.schedule_wrap(function(new_config, new_root_dir)
+				local _virtual_env
+				(function(root_dir)
+					local pipenv_dir
+
+					local pipenv_match = vim.fn.glob(require 'lspconfig.util'.path.join(root_dir, 'Pipfile.lock'))
+					if pipenv_match ~= '' then
+						pipenv_dir = pipenv_venv_cached(root_dir)
+					end
+
+					if not vim.env.VIRTUAL_ENV or vim.env.VIRTUAL_ENV == '' then
+						_virtual_env = pipenv_dir
+					end
+
+					if _virtual_env ~= '' then
+						vim.env.VIRTUAL_ENV = _virtual_env
+						vim.env.PATH = require 'lspconfig.util'.path.join(_virtual_env, 'bin:') .. vim.env.PATH
+					end
+
+					if _virtual_env ~= '' and vim.env.PYTHONHOME then
+						vim.env.old_PYTHONHOME = vim.env.PYTHONHOME
+						vim.env.PYTHONHOME = ''
+					end
+
+					return _virtual_env ~= '' and require 'lspconfig.util'.path.join(_virtual_env, 'bin:') .. vim.env.PATH or
+							''
+				end)(new_root_dir)
+
+				new_config.settings.python.pythonPath = vim.fn.exepath 'python'
+			end),
+		})
+	end,
+}
+
 return {
 	{
 		'WhoIsSethDaniel/mason-tool-installer.nvim',
@@ -6,16 +106,7 @@ return {
 			'williamboman/mason.nvim',
 		},
 		opts = {
-			ensure_installed = {
-				'prettierd',
-				'jsonls',
-				'pyright',
-				'shfmt',
-				'lua-language-server',
-				'typescript-language-server',
-				'pyright',
-				'ruff',
-			},
+			ensure_installed = ensure_installed,
 			auto_update = false,
 			run_on_start = false,
 		},
@@ -54,19 +145,6 @@ return {
 			local mason_lspconfig = require 'mason-lspconfig'
 			mason_lspconfig.setup()
 
-			-- used by pyright
-			-- this could potentially be in the python setup handler,
-			-- need to research when those are invoked.
-			local root_dir_cache = {}
-			local pipenv_venv_cached = function(root_dir)
-				if root_dir_cache[root_dir] then
-					return root_dir_cache[root_dir]
-				end
-				local value = vim.fn.trim(vim.system({ 'pipenv', '--venv', '--quiet' }, { text = true }):wait().stdout)
-				root_dir_cache[root_dir] = value
-				return value
-			end
-
 			local lspconfig = require 'lspconfig'
 
 			local default_capabilities_opts = {
@@ -75,85 +153,14 @@ return {
 			local mix_capabilities = function(opts)
 				return vim.tbl_deep_extend('keep', opts, default_capabilities_opts)
 			end
-			mason_lspconfig.setup_handlers {
-				function(server_name)
+			local servers = mason_lspconfig.get_installed_servers()
+			for _, server_name in ipairs(servers) do
+				if custom_lspconfig[server_name] ~= nil then
+					custom_lspconfig[server_name](mix_capabilities)
+				else
 					lspconfig[server_name].setup(mix_capabilities {})
-				end,
-
-				lua_ls = function()
-					local runtime_path = vim.split(package.path, ';')
-					table.insert(runtime_path, 'lua/?.lua')
-					table.insert(runtime_path, 'lua/?/init.lua')
-
-					lspconfig.lua_ls.setup(mix_capabilities {
-						settings = {
-							Lua = {
-								telemetry = { enable = false },
-								runtime = {
-									version = 'LuaJIT',
-									path = runtime_path,
-								},
-								diagnostics = {
-									globals = { 'vim' }
-								},
-								workspace = {
-									checkThirdParty = false,
-									library = {
-										vim.fn.expand('$VIMRUNTIME/lua'),
-										vim.fn.stdpath('config') .. '/lua'
-									}
-								}
-							}
-						}
-					})
-				end,
-
-				pyright = function()
-					-- condensed form of this:
-					-- https://github.com/younger-1/nvim/blob/one/lua/young/lsp/providers/pyright.lua
-					require 'lspconfig'.pyright.setup(mix_capabilities {
-						settings = {
-							python = {
-								analysis = {
-									useLibraryCodeForTypes = true,
-									autoImportCompletions = true,
-									autoSearchPaths = true,
-								},
-							},
-						},
-						on_new_config = vim.schedule_wrap(function(new_config, new_root_dir)
-							local _virtual_env
-							(function(root_dir)
-								local pipenv_dir
-
-								local pipenv_match = vim.fn.glob(require 'lspconfig.util'.path.join(root_dir, 'Pipfile.lock'))
-								if pipenv_match ~= '' then
-									pipenv_dir = pipenv_venv_cached(root_dir)
-								end
-
-								if not vim.env.VIRTUAL_ENV or vim.env.VIRTUAL_ENV == '' then
-									_virtual_env = pipenv_dir
-								end
-
-								if _virtual_env ~= '' then
-									vim.env.VIRTUAL_ENV = _virtual_env
-									vim.env.PATH = require 'lspconfig.util'.path.join(_virtual_env, 'bin:') .. vim.env.PATH
-								end
-
-								if _virtual_env ~= '' and vim.env.PYTHONHOME then
-									vim.env.old_PYTHONHOME = vim.env.PYTHONHOME
-									vim.env.PYTHONHOME = ''
-								end
-
-								return _virtual_env ~= '' and require 'lspconfig.util'.path.join(_virtual_env, 'bin:') .. vim.env.PATH or
-										''
-							end)(new_root_dir)
-
-							new_config.settings.python.pythonPath = vim.fn.exepath 'python'
-						end),
-					})
-				end,
-			}
+				end
+			end
 
 			local cmp = require 'cmp'
 			cmp.setup {
