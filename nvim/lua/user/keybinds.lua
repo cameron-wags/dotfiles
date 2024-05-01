95dd8f2c58260c72cc2e17318ce5b253476b691f
Cameron Wagstaff <cam.wagstaff@gmail.com>
2024-04-30 22:44:54 -0500

    tidy up keybinds

--- nvim/lua/user/keybinds.lua
+++ nvim/lua/user/keybinds.lua
@@ -13,7 +13,17 @@ local vn = mkmap('v', true)
 local ino = mkmap('i', true)
 local tma = mkmap('t', false)
 
---Clear hlsearch with return
+local function make_lazy_require_module(mod)
+	return setmetatable({}, {
+		__index = function(_, k)
+			return function(...)
+				local theargs = { ... }
+				return function() require(mod)[k](unpack(theargs)) end
+			end
+		end
+	})
+end
+
 nn('<leader><CR>', '<Cmd>noh<CR>', 'Clear search highlights')
 
 nm('<C-/>', 'gcc', 'Comment - toggle current line')
@@ -25,65 +35,48 @@ nn('<leader>q', '<Cmd>bp|bd!#<CR>', 'Buffer - close without saving')
 nn('<leader>w', '<Cmd>w<CR>', 'Buffer - Write')
 nn('<leader>t', '<Cmd>enew<CR>', 'Buffer - create new')
 
--- Resize with arrows
 nn('<M-Up>', '<Cmd>resize +2<CR>', 'Split - make taller')
 nn('<M-Down>', '<Cmd>resize -2<CR>', 'Split - make shorter')
 nn('<M-Left>', '<Cmd>vertical resize -2<CR>', 'Split - make narrower')
 nn('<M-Right>', '<Cmd>vertical resize +2<CR>', 'Split - make wider')
 
--- Change windows
 nn('<M-h>', '<Cmd>winc h<CR>', 'Window - navigate left')
 nn('<M-j>', '<Cmd>winc j<CR>', 'Window - navigate down')
 nn('<M-k>', '<Cmd>winc k<CR>', 'Window - navigate up')
 nn('<M-l>', '<Cmd>winc l<CR>', 'Window - navigate right')
 
--- Moving the selected line from Visual Mode
 vn('J', ":m '>+1<CR>gv=gv", 'Move visual selection down')
 vn('K', ":m '<-2<CR>gv=gv", 'Move visual selection up')
+vn('<', '<gv', 'Unindent visual selection')
+vn('>', '>gv', 'Indent visual selection')
 
--- Integrated terminal
-local fterm_lazygit = nil
 nn('<M-;>', function()
-	if fterm_lazygit then
-		fterm_lazygit:close()
-	end
 	require 'FTerm'.open()
-end, 'FTerm - toggle')
-
+end, 'FTerm - open')
 tma('<M-;>', function()
-	if fterm_lazygit then
-		fterm_lazygit:close()
-		vim.schedule(function()
-			vim.cmd.checktime()
-		end)
-	end
 	require 'FTerm'.close()
-end, 'FTerm - toggle')
+	vim.schedule(function()
+		vim.cmd.checktime()
+	end)
+end, 'FTerm - close')
 
 nn('<leader>g', '<cmd>:Git<CR>', 'Fugitive - open')
-
 nn('<leader>o', function() require 'oil'.open_float() end, 'Oil - open')
+nn('<leader>e', '<Cmd>NvimTreeToggle<CR>', 'NvimTree - toggle')
 
--- Stay in indent mode
-vn('<', '<gv', 'Unindent line(s)')
-vn('>', '>gv', 'Indent lines(s)')
 
--- Telescope formatters
-local function tscope(fn)
-	return function()
-		require 'telescope.builtin'[fn]()
+nn('gx', function()
+	local url = vim.fn.expand("<cfile>")
+	if vim.fn.has('mac') then
+		vim.system({ 'open', url }, { detach = true })
+	elseif vim.fn.has('unix') then
+		vim.system({ 'xdg-open', url }, { detach = true })
+	else
+		vim.api.nvim_notify("It's time to set up open again", vim.log.levels.WARN, {})
 	end
-end
+end, 'Open URL under cursor')
 
--- open urls under the cursor
-if vim.fn.has('mac') then
-	nn('gx', '<Cmd>call jobstart(["open", expand("<cfile>")], {"detach": v:true})<CR>', 'Open URL under cursor')
-elseif vim.fn.has('unix') then
-	nn('gx', '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>', 'Open URL under cursor')
-else
-	vim.api.nvim_notify("It's time to set up open again", vim.log.levels.WARN, {})
-end
-
+-- keep an eye on this one
 local function close_floats()
 	for _, win in ipairs(vim.api.nvim_list_wins()) do
 		if vim.api.nvim_win_get_config(win).relative == "win" then
@@ -91,27 +84,24 @@ local function close_floats()
 		end
 	end
 end
--- keep an eye on this one
 nn('<Esc>', close_floats, 'Close floats', { noremap = nil })
 
-nn('<leader>e', '<Cmd>NvimTreeToggle<CR>', 'NvimTree - toggle')
+local tscope = make_lazy_require_module 'telescope.builtin'
+nn('<leader>,', tscope.buffers(), 'Telescope - buffers')
+nn('<leader>.', tscope.find_files(), 'Telescope - project files')
+nn('<leader>fr', tscope.oldfiles(), 'Telescope - oldfiles')
+nn('<leader>fg', tscope.live_grep(), 'Telescope - grep in project')
+nn('<leader>fh', tscope.help_tags(), 'Telescope - vim help')
 
-nn('<leader>,', tscope 'buffers', 'Telescope - buffers')
-nn('<leader>.', tscope 'find_files', 'Telescope - project files')
-nn('<leader>fr', tscope 'oldfiles', 'Telescope - oldfiles')
-nn('<leader>fg', tscope 'live_grep', 'Telescope - grep in project')
-nn('<leader>fh', tscope 'help_tags', 'Telescope - vim help')
-
--- lsp related keybinds
-nn('gr', tscope 'lsp_references', 'Telescope - current symbol references')
-nn('gd', tscope 'lsp_definitions', 'Telescope - current symbol definitions')
-nn('gs', tscope 'lsp_document_symbols', 'Telescope - current file symbols')
+nn('gr', tscope.lsp_references(), 'Telescope - current symbol references')
+nn('gd', tscope.lsp_definitions(), 'Telescope - current symbol definitions')
+nn('gs', tscope.lsp_document_symbols(), 'Telescope - current file symbols')
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(event)
 		local opts = { buffer = event.buf }
 
-		nn('K', vim.lsp.buf.hover, 'LSP Hover', opts)
+		nn('K', vim.lsp.buf.hover, 'LSP - Hover', opts)
 		nn('gD', vim.lsp.buf.declaration, 'Goto declaration', opts)
 		nn('gi', vim.lsp.buf.implementation, 'Goto implementation', opts)
 		nn('go', vim.lsp.buf.type_definition, 'Goto type definition', opts)
@@ -143,38 +133,38 @@ nn('<leader>D', function()
 	end
 end, 'Diagnostics - enable lsp_lines')
 
--- git stuff
+local gitsigns = make_lazy_require_module 'gitsigns'
 nn(']c', function()
 	if vim.wo.diff then
 		vim.cmd.normal({ ']c', bang = true })
 	else
-		require 'gitsigns'.nav_hunk('next', { preview = true, })
-		vim.api.nvim_feedkeys('zz', 'n', false)
+		gitsigns.nav_hunk('next', { preview = true })()
+		vim.cmd.normal({ 'zz', bang = true })
 	end
-end)
+end, 'Git - Goto next hunk')
 
 nn('[c', function()
 	if vim.wo.diff then
 		vim.cmd.normal({ '[c', bang = true })
 	else
-		require 'gitsigns'.nav_hunk('prev', { preview = true, })
-		vim.api.nvim_feedkeys('zz', 'n', false)
+		gitsigns.nav_hunk('prev', { preview = true, })()
+		vim.cmd.normal({ 'zz', bang = true })
 	end
-end)
+end, 'Git - Goto previous hunk')
 
 nn('<leader>hl', function()
-		require 'gitsigns'.refresh()
-		require 'gitsigns'.setqflist('all', { use_location_list = true })
+		gitsigns.refresh()()
+		gitsigns.setqflist('all', { use_location_list = true })()
 	end,
 	'Gitsigns - Location list hunks')
 nn('<leader>s', function()
-	require 'gitsigns'.stage_hunk()
+	gitsigns.stage_hunk()()
 	close_floats()
 end, 'Gitsigns - Stage hunk')
-vn('<leader>s', function() require 'gitsigns'.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+vn('<leader>s', gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') },
 	'Gitsigns - Stage range')
-nn('<leader>S', function() require 'gitsigns'.stage_buffer() end, 'Gitsigns - Stage buffer')
-nn('<leader>hu', function() require 'gitsigns'.undo_stage_hunk() end, 'Gitsigns - Undo last stage hunk')
-nn('<leader>hr', function() require 'gitsigns'.reset_hunk() end, 'Gitsigns - Reset hunk')
-vn('<leader>hr', function() require 'gitsigns'.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+nn('<leader>S', gitsigns.stage_buffer(), 'Gitsigns - Stage buffer')
+nn('<leader>hu', gitsigns.undo_stage_hunk(), 'Gitsigns - Undo last stage hunk')
+nn('<leader>hr', gitsigns.reset_hunk(), 'Gitsigns - Reset hunk')
+vn('<leader>hr', gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') },
 	'Gitsigns - Reset range')
